712161459581be11cc1fad8ad4a61986fe2fff34
MiguelSernaM <mangel.serna@udea.edu.co>
2021-09-20 20:56:45 -0500

    Se corrigio error de el 255 en tinkercad y nuevo base para funcion de escalado

--- Codigo Qt/parcial2Qt/escalado.cpp
+++ Codigo Qt/parcial2Qt/escalado.cpp
@@ -14,56 +14,46 @@ void escalado::asiggnVect(vector< vector< vector<int> > > vec3d)
     vec3d2 = vec3d;
 }
 
-void escalado::sobremuestrear( int signox,int signoy)
+void escalado::escalameEsta()
 {
-//supongamos que esta es la de maximizar pq no me acuerdo si era la otra xD
-    //Tengo que ver la proporción de cuanto me falta y reemplazar ese valor en un for para llenar pixeles
 
-
-}
-
-void escalado::submuestrar(int signox,int signoy)
-{
-    //Con usted mijita tengo que ver es cuanto me esta sobrando, dividirlo por 8 creo xD y sacar promedio de colores
-}
-//funcion encargada de detectarme las diferencas uwu
-void escalado::detectDiference()
-{
     //es mas chiquita en los dos ejes x< y<
     if(anchoLectura < valex && altoLectura < valey){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas chiquita en solo el eje x e igual en el y x< y==
     else if(anchoLectura < valex && altoLectura == valey){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas chiquita en solo el eje x y es mas grande en y x< y>
     else if (anchoLectura < valex && altoLectura > valey){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas chiquita en solo el eje y e igual en el x y< x==
     else if(altoLectura < valey && anchoLectura == valex){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas chiquita en solo el eje y y mas grande en x y< x>
     else if(altoLectura < valey && anchoLectura > valex){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas grande en los dos ejes y> x>
     else if(anchoLectura > valex && altoLectura > valey){
-
+        vector<vector<vector<int>>> vectTemp;
     }
     //es mas grande solo en x y igual en y x> y==
     else if(anchoLectura > valex && altoLectura == valey){
-
+        vector<vector<vector<int>>> vectTemp;
     }
 
     //es mas grande solo en y y igual en x y> x==
     else if(altoLectura > valey && anchoLectura == valex){
-
+        vector<vector<vector<int>>> vectTemp;
     }
 
+
 }
+
 
 escalado::~escalado()
 {
--- Codigo Qt/parcial2Qt/lecturaescritura.cpp
+++ Codigo Qt/parcial2Qt/lecturaescritura.cpp
@@ -21,21 +21,30 @@ lecturaEscritura::lecturaEscritura(QString nombre)
     void lecturaEscritura::escribirDatos()
 {
     vector< vector< vector<int> > > arreglo3d(ancho, vector< vector<int> >(alto , vector<int>(color)));
-
+    int tempInt;
     for(int i=0;i<ancho;++i){
         for(int j=0;j<alto;++j){
             for(int k=0;k<color;++k){
                 switch (k) {
                 case 0:{
-                    arreglo3d[i][j][k] = imagenFuente->pixelColor(i,j).red();
+                    tempInt = imagenFuente->pixelColor(i,j).red();
+                    if(tempInt == 255)
+                        tempInt--;
+                    arreglo3d[i][j][k] = tempInt;
                     break;
                 }
                 case 1:{
-                    arreglo3d[i][j][k] = imagenFuente->pixelColor(i,j).green();
+                    tempInt = imagenFuente->pixelColor(i,j).green();
+                    if(tempInt == 255)
+                        tempInt--;
+                    arreglo3d[i][j][k] = tempInt;
                     break;
                 }
                 case 2:{
-                    arreglo3d[i][j][k] = imagenFuente->pixelColor(i,j).blue();
+                    tempInt = imagenFuente->pixelColor(i,j).blue();
+                    if(tempInt == 255)
+                        tempInt--;
+                    arreglo3d[i][j][k] = tempInt;
                     break;
                 }
                 }
